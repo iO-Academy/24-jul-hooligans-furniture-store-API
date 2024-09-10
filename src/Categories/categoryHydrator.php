38d40cde8106781c86db39353c72e1cb48fd030d
Thomas King <thomasgking42@gmail.com>
2024-09-10 12:11:34 +0100

    peer review

--- src/Categories/categoryHydrator.php
+++ src/Categories/categoryHydrator.php
@@ -6,7 +6,7 @@ class CategoryHydrator
 {
     public static function getCategory(PDO $db)
     {
-            $query = $db->prepare("SELECT `id`, `name` FROM `Categories`");
+            $query = $db->prepare("SELECT `id`, `name`, `products` FROM `Categories`");
             $query->execute();
             $query->setFetchMode(PDO::FETCH_CLASS, Category::class);
             return $query->fetchAll();
