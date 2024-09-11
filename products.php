0741b8fed9150497928863dd0dabcdc68bd9b40a
Thomas King <thomasgking42@gmail.com>
2024-09-11 09:33:31 +0100

    peer review ammends

--- products.php
+++ products.php
@@ -9,16 +9,20 @@ use FurnitureStoreAPI\Response\ResponseService as Response;
 use FurnitureStoreAPI\Services\Headers as SetHeaders;
 
 SetHeaders::apiHeaders();
-    try {
-        if (isset($_GET['cat']) && $_GET['cat'] < 12 && $_GET['cat'] > 0) {
-            echo Response::apiResponse(200, 'Successfully retrieved products', ProductsHydrator::getProducts(Connection::db(), intval($_GET['cat'])));
+    try
+    {
+        if (isset($_GET['cat']) && $_GET['cat'] < 12 && $_GET['cat'] > 0)
+        {
+            echo Response::apiResponse(200, 'Successfully retrieved products',
+                ProductsHydrator::getProducts(Connection::db(), intval($_GET['cat'])));
         }
         else
         {
             echo Response::apiResponse(400, 'Invalid category id', []);
         }
-    } catch (Exception $e)
-    {
-        echo Response::apiResponse(500, 'Unexpected error', []);
-        ErrorLogging::errorLogging($e);
     }
+    catch (Exception $e)
+        {
+            echo Response::apiResponse(500, 'Unexpected error', []);
+            ErrorLogging::errorLogging($e);
+        }
--- src/Products/ProductsHydrator.php
+++ src/Products/ProductsHydrator.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace FurnitureStoreAPI\Products;
+
 use \PDO;
+
 class ProductsHydrator
 {
     public static function getProducts(PDO $db, int $categoryID)
     {
-        $query = $db->prepare("SELECT `categoryID`, `price`, `stock`, `color` FROM `Products` 
-                                                                                WHERE `categoryID` = :categoryID");
+        $query = $db->prepare("SELECT `categoryID`, `price`, `stock`, `color` 
+                                FROM `Products` WHERE `categoryID` = :categoryID");
         $query->bindParam(':categoryID', $categoryID);
         $query->execute();
         $query->setFetchMode(PDO::FETCH_CLASS, Products::class);
