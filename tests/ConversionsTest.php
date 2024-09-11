ff0b6ffca13223e61dc357b328668ceb81d80090
Ramsey Merdassi <merdassi.r@gmail.com>
2024-09-11 16:24:02 +0100

    added unit tests

--- tests/ConversionsTest.php
+++ tests/ConversionsTest.php
@@ -1,32 +1,23 @@
 <?php
 
-use FurnitureStoreAPI\Services\uomConversions as Conversions;
+use FurnitureStoreAPI\Services\UOMConversionService as UnitConversionService;
 use PHPUnit\Framework\TestCase;
 
 class ConversionsTest extends TestCase
 {
     public function testCMConversion_success()
     {
-        $result = Conversions::cmConversion(100);
-        $this->assertSame(10.0, $result);
+        $_GET['unit'] = 'ft';
+        $measurement = new UnitConversionService();
+        $result = $measurement::convertUnit(100);
+        $this->assertSame(0.33, $result);
     }
 
     public function testCMConversion_malformed()
-{
-    $this->expectException(TypeError::class);
-    $result = Conversions::cmConversion([100]);
-}
-
-    public function testInchConversion_success()
     {
-        $result = Conversions::inchConversion(100);
-        $this->assertSame(10.0, $result);
-    }
-
-    public function testInchConversion_malformed()
-    {
+        $_GET['unit'] = 'ft';
+        $measurement = new UnitConversionService();
         $this->expectException(TypeError::class);
-        $result = Conversions::cmConversion([100]);
+        $result = $measurement::convertUnit([100]);
     }
-
 }
