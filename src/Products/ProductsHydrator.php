708b51567a504a3518dd1dbf366246d291ed44a6
Ramsey Merdassi <merdassi.r@gmail.com>
2024-09-11 11:26:16 +0100

    added new product method to ProductsHydrator class

--- src/Products/ProductsHydrator.php
+++ src/Products/ProductsHydrator.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace FurnitureStoreAPI\Products;
+use FurnitureStoreAPI\Product\Product;
 use \PDO;
 class ProductsHydrator
 {
@@ -13,4 +14,15 @@ class ProductsHydrator
         $query->setFetchMode(PDO::FETCH_CLASS, Products::class);
         return $query->fetchAll();
     }
+
+    public static function getProduct(PDO $db, int $productID)
+    {
+        $query = $db->prepare("SELECT `id`, `categoryID`, `width`, `height`, `depth`, `price`, `stock`,
+        `related`, `color` FROM `Products` WHERE `id` = :productID");
+        $query->bindParam(':productID', $productID);
+        $query->execute();
+        $query->setFetchMode(PDO::FETCH_CLASS, Product::class);
+        return $query->fetch();
+    }
+
 }
